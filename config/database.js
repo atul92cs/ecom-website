ebbef689035ea4549676ef0e99c2146f777274fd
Atul <atul.11192@gmail.com>
2023-01-06 02:13:14 +0530

    adding code for the master branch

---
+++ config/database.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const {Sequelize, Model}=require('sequelize');
+const sequelize=new Sequelize(process.env.SCHEMA,process.env.USER,process.env.PASSWORD,{
+    dialect:'postgres',
+    port:5432,
+    host:process.env.HOST
+});
+
+module.exports=sequelize;
---
+++ helpers/file-uploader.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const cloudinary=require('cloudinary').v2;
+
+const multer = require('multer');
+let {CloudinaryStorage}=require('multer-storage-cloudinary');
+
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET
+});
+
+const storage=new CloudinaryStorage({
+    cloudinary:cloudinary,
+    params:{
+        folder:"ecom",
+        public_id:(req,file)=>file.filename,
+    },
+});
+
+const parser=multer({storage:storage});
+
+module.exports=parser;
